2ce82ecbf9e33924305a8f3cef68b3551df738da
suenaga11 <a.suenaga.987@nitech.jp>
2020-06-17 02:23:07 +0900

    教室選択ページを仮移植

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
     def index
     end
 
+    def select_room
+    end
+
     def test
         @test = params[:id]
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root 'home#index'
+  get '/select_room', to: 'home#select_room'
   get '/test/:id', to: 'home#test'
 end
