585270c553d5b8932fe11deca0ecc40fcb3b5e72
suenaga11 <a.suenaga.987@nitech.jp>
2020-06-18 08:59:26 +0900

    ページごとのタイトルを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    # ページごとのタイトル
+    def full_title(page_title = '')
+      base_title = "Kids RESAS"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+    end
+
 end
