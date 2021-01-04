9b399f65927b9539865008aaf269f7f2478a1b4a
akamiya208 <a.kamiya.208@gmail.com>
2021-01-04 08:46:32 +0000

    add 同期処理アルファ

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,15 @@
 class RoomChannel < ApplicationCable::Channel
     def subscribed
       stream_from "room_channel_#{params['room_id']}"
+
+
+      redis = Redis.new(host: ENV["REDIS_HOST"], port: ENV["REDIS_PORT"])
+      if redis.get(params['room_id']).nil?
+        dropItems = []
+      else
+        dropItems = JSON.parse(redis.get(params['room_id']))
+      end
+      ActionCable.server.broadcast("room_channel_#{params['room_id']}", dropItems)
     end
   
     def unsubscribed
@@ -9,8 +18,22 @@ class RoomChannel < ApplicationCable::Channel
   
 
     def receive(data)
-        # TODO: 保存処理書くかも
         # サーバーでデータを受信した時
-        ActionCable.server.broadcast("room_channel_#{params['room_id']}", data)
+        redis = Redis.new(host: ENV["REDIS_HOST"], port: ENV["REDIS_PORT"])
+        if data["operation"] == "deleteAll" then
+          unless redis.get(params['room_id']).nil? then
+            redis.del params['room_id']
+          end
+          dropItems = []
+        elsif data["operation"] == "add" then
+          if redis.get(params['room_id']).nil?
+            dropItems = []
+          else
+            dropItems = JSON.parse(redis.get(params['room_id']))
+          end
+          dropItems.push data["data"]
+          redis.set params['room_id'], dropItems.to_json
+        end
+        ActionCable.server.broadcast("room_channel_#{params['room_id']}", dropItems)
     end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,14 +2,14 @@ import consumer from "./consumer"
 
 window.addEventListener('load', function () {
     // TODO: See: https://railsguides.jp/action_cable_overview.html
-    const roomChannel = consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
+    roomChannel = consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
         initialized() {
             // 最初に呼び出される
-            console.log("初期化");
         },
         received(data) {
             // データを受信したときに呼び出し
-            console.log(data);
+            ItemStatus = data;
+            drawItem();
         },
         disconnected() {
             // WebSocketコネクションがクローズすると呼び出される
@@ -18,20 +18,4 @@ window.addEventListener('load', function () {
             // サブスクリプションがサーバーで却下されると呼び出される
         },
     })
-
-    // TODO: See: https://developer.mozilla.org/ja/docs/Web/API/HTML_Drag_and_Drop_API
-    let squareFrame = document.getElementById("square-frame")
-
-    squareFrame.addEventListener('click', function (event) {
-        //TODO: https://murashun.jp/blog/20191110-48.html
-        roomChannel.send({ offsetX: event.offsetX, offsetY: event.offsetY })
-    })
-
-    let items = document.getElementById("light_bl")
-
-    items.addEventListener('dragend', function (event) {
-        roomChannel.send({ offsetX: event.offsetX, offsetY: event.offsetY })
-    })
-
-    // 
 })
